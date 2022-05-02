4e1aaa1ad6ccac9bcc44cf7c0323c90f2c536799
PaRang96 <winterresie94@gmail.com>
2022-05-02 15:18:16 +0900

    이용자가 입력 가능하게 설정

--- main.cpp
+++ main.cpp
@@ -3,8 +3,12 @@ using namespace std;
 
 int main()
 {
+	int Number = 0;
+
+	cin >> Number;
+
 	char star = '*';
-	for (int j = 1; j <= 5; j++)
+	for (int j = 1; j <= Number; j++)
 	{
 		for (int i = 0; i < j; i++)
 		{
