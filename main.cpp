679882e9f8b0d6fb5ae6fa5e8c377bc02580bd57
PaRang96 <winterresie94@gmail.com>
2022-05-02 09:19:16 +0900

    first commit

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b = 0;
+	float c, d = 0;
+
+	a = 3, b = 5;
+	c = 3.05, d = 2.78;
+
+	cout << a + b << endl;
+	cout << a - b << endl;
+	cout << a * b << endl;
+	cout << a / b << endl;
+	cout << a % b << endl << endl;
+
+	cout << c + d << endl;
+	cout << c - d << endl;
+	cout << c * d << endl;
+	cout << c / d << endl;
+	cout << c % d << endl;
+	//
+
+	return 0;
+}
