bb97dd76973fbfa35c90e60a16a5540d619b491f
PaRang96 <winterresie94@gmail.com>
2022-05-02 15:13:53 +0900

    별 갯수를 한 줄에 1 2 3 4 5

--- main.cpp
+++ main.cpp
@@ -4,9 +4,9 @@ using namespace std;
 int main()
 {
 	char star = '*';
-	for (int j = 0; j < 5; j++)
+	for (int j = 1; j <= 5; j++)
 	{
-		for (int i = 0; i < 5; i++)
+		for (int i = 0; i < j; i++)
 		{
 			cout << star;
 		}
