1b749cc748e96ae8d4ac545a797e0baef6db8137
PaRang96 <winterresie94@gmail.com>
2022-05-02 15:04:01 +0900

    별 다섯개 다섯줄

--- main.cpp
+++ main.cpp
@@ -4,10 +4,13 @@ using namespace std;
 int main()
 {
 	char star = '*';
-
-	for (int i = 0; i < 5; i++)
+	for (int j = 0; j < 5; j++)
 	{
-		cout << star;
+		for (int i = 0; i < 5; i++)
+		{
+			cout << star;
+		}
+		cout << endl;
 	}
 	return 0;
 }
